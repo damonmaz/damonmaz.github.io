850ce3cdcd7cd083a4e1e83c548490066fef1c8f
damonmaz <>
2025-07-04 18:45:49 -0600

    Revert "add basic navbar"
    This reverts commit a3651e0c8b0ef0e2937b3b58ca89a6bdf072ae99.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,7 @@
 <script lang="ts">
-    import '../app.css';
-    import TitleZoom from '$lib/components/TitleZoom.svelte';
-    import NavBar from '$lib/components/NavBar.svelte';
-    import Main from '$lib/components/Main.svelte';
-    import { onMount } from 'svelte';
-    let showNav = false;
-    let titleProgress = 0;
-
-	const DURATION: number = 2000; // Duration for the title zoom animation in milliseconds
-
-    onMount(() => {
-        const timeout = setTimeout(() => {
-            showNav = true;
-        }, DURATION);
-        return () => clearTimeout(timeout);
-    });
+	import '../app.css';
+	
+	let { children } = $props();
 </script>
 
-<TitleZoom
-  text="Damon Mazurek"
-  duration={DURATION}
-  on:progress={e => titleProgress = e.detail.progress}
-/>
-
-<div class="flex justify-center w-full mt-8 xl:mt-12 pt-30">
-    <NavBar {showNav} show={showNav} />
-</div>
-
-<Main className="" {titleProgress}>
-  <slot />
-</Main>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script>
-    import TitleZoom from '$lib/components/TitleZoom.svelte';
-    import Main from '$lib/components/Main.svelte';
-</script>
-
-<Main>
-    
-</Main>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
